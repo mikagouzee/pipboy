b7ccfc2e8e6a01800bc2f75978f83c9f090fbf21
NotRelevant <michel.gouzee@hotmail.com>
2026-01-29 19:23:24 +0100

    added ansible playbook to prep the pi for future instances and kiosk service for display

--- src/scripts/launch_pipboy.sh
+++ src/scripts/launch_pipboy.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+#Environment setup
+export DISPLAY=:0
+export URL="http://localhost:5000"
+
 # Prevent screen from dimming/sleeping
 xset s off
 xset -dpms
 xset s noblank
 
 # Wait for the backend
-until $(curl --output /dev/null --silent --head --fail http://localhost:5000); do
+echo "Waiting for backend to be available at $URL..."
+until curl --output /dev/null --silent --head --fail "$URL"; do
     sleep 1
 done
 
+# Kill any existing Chromium instances
+pkill chromium || echo "No existing Chromium instances to kill."
+
 # Launch Chromium (Targeting the specific X11 display if needed)
 # On SPI screens, you might need to specify DISPLAY=:0
+# remote 9222 port is for debugging purposes
 export DISPLAY=:0
-chromium --kiosk --noerrdialogs --disable-infobars http://localhost:5000
+chromium  \
+    --kiosk \
+    --no-first-run \
+    --noerrdialogs \
+    --disable-infobars \
+    --disable-session-crashed-bubble \
+    --check-for-update-interval=31536000 \
+    --remote-debugging-port=9222 \
+    "$URL"
